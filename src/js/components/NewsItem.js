6ef29f2fd56d165debd578d4d4883b67d2d28cf6
Itunu Fatoki <itunuworks@gmail.com>
2017-04-30 00:19:23 +0100

    Implement reading of news article in new tab based on click event

--- src/js/components/NewsItem.js
+++ src/js/components/NewsItem.js
@@ -2,11 +2,16 @@ import React from 'react';
 
 import newsStore from '../store/newsStore';
 import * as newsActions from '../actions/newsActions';
-// import Dropdownitem from './Dropdownitem';
 
 export default class Articles extends React.Component{
   constructor(props) {
     super();
+  }
+
+  handleClick(e) {
+    const { url } = this.props;
+
+    window.open(url,'_blank');
   }
 
   render() {
@@ -15,7 +20,7 @@ export default class Articles extends React.Component{
     return (
       <li>
         <div>
-          <a href={url}>{title}</a>
+          <a href='#' onClick={this.handleClick.bind(this)}>{title}</a>
           <div>{description}</div>
           <div>Author: {author}</div>
           <div>Posted On: {publishedAt}</div>
@@ -25,3 +30,8 @@ export default class Articles extends React.Component{
   }
 }
 
+// function handleClick(e) {
+//   window.open('http://www.aljazeera.com/news/2017/04/philippine-leader-korea-world-170429145920088.html','_blank');
+// }
+
+// function(){window.open('www.yourdomain.com','_blank')}

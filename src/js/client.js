d03da888b5b597ad583e52956a03d0d80f0931ce
itunuworks <itunuworks@users.noreply.github.com>
2017-04-30 20:06:59 +0100

    Revert "Basic functionality branch"

--- src/js/client.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Router, Route, IndexRoute, hashHistory } from 'react-router';
-
-import Layout from './pages/layout';
-
-const app = document.getElementById('app');
-ReactDOM.render(<Layout/>, app);

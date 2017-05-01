532eb81419a1574ea98ff34393cea4e1e297f4d9
Itunu Fatoki <itunuworks@gmail.com>
2017-05-01 04:11:33 +0100

    Setup Google+ authentication using react-google-login

--- src/js/pages/layout.js
+++ src/js/pages/layout.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Sources from '../components/Sources';
 import Articles from '../components/Articles';
+import GoogleLogin from 'react-google-login';
 
 export default class Layout extends React.Component{
   constructor() {
@@ -12,6 +13,12 @@ export default class Layout extends React.Component{
     return (
       <div>
         <h1>Hello Everyone, I am a news apps.</h1>
+        <GoogleLogin
+          clientId='5132228650-521v0qouoj8iv6uqr6od6m8v9hiv906m.apps.googleusercontent.com'
+          buttonText="Login"
+          onSuccess={responseGoogleSuccess}
+          onFailure={responseGoogleFailure}
+        />
         <Sources/>
         <Articles/>
       </div>
@@ -19,3 +26,10 @@ export default class Layout extends React.Component{
   }
 }
 
+const responseGoogleSuccess = (response) => {
+  console.log(`Logged in as : ${response.profileObj.name}`);
+}
+
+const responseGoogleFailure = (response) => {
+  console.log(response);
+}

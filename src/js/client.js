6616b3c26a070dcab86a5dd8d8c95dc655fe72ce
Andela Developer <andeladeveloper@Andelas-MacBook-Pro.local>
2017-05-04 17:37:46 +0100

    Implement some code refactoring

--- src/js/client.js
+++ src/js/client.js
@@ -1,16 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { 
-  HashRouter, 
-  Route, 
-} from 'react-router-dom';
-import { 
-  Router,
-  IndexRoute, 
-  hashHistory,
+import {
+  HashRouter,
+  Route,
   Switch,
-} from 'react-router';
-
+} from 'react-router-dom';
 
 import Layout from './pages/layout';
 import Home from './pages/Home';
@@ -21,10 +15,10 @@ ReactDOM.render(
   <HashRouter>
     <Layout>
       <Switch>
-        <Route path='/home' component={Home}/>
-        <Route path='/main' component={Main}/>
+        <Route path="/home" component={Home} />
+        <Route path="/main" component={Main} />
       </Switch>
     </Layout>
   </HashRouter>,
-	app
+	app,
 );

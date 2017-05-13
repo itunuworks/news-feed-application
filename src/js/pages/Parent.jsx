f416967317b1d4df383962d158e5e6b096fbc56d
Itunuloluwa Fatoki <itunuloluwa.fatoki@andela.com>
2017-05-13 17:20:03 +0100

    Fix chmodifications to the ListView component and Parent

--- src/js/pages/Parent.jsx
+++ src/js/pages/Parent.jsx
@@ -8,9 +8,11 @@ import {
 import Home from './Home';
 import Main from './Main';
 import Layout from './Layout';
+import ListView from './ListView';
 import newsStore from '../store/newsStore';
 
-// Create a Parent component which helps with housing other components and front-end routing.
+// Create a Parent component which helps with
+// housing other components and front-end routing.
 export default class Parent extends React.Component {
   constructor() {
     super();
@@ -46,8 +48,11 @@ export default class Parent extends React.Component {
       <HashRouter>
         <Layout>
           <Switch>
-            <Route path="/" component={this.state.user ? Main : Home} />
+            <Route path="/home" component={this.state.user ? Main : Home} />
             <Route path="/main" component={this.state.user ? Main : Home} />
+            <Route
+              path="/favorites" component={this.state.user ? ListView : Home}
+            />
           </Switch>
         </Layout>
       </HashRouter>

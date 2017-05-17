6c7b5809b030295e6114585c66d204b0aebc5b50
Itunuloluwa Fatoki <itunuloluwa.fatoki@andela.com>
2017-05-17 17:18:44 +0100

    store test mocked and passes 100%

--- __tests__/actions/newsActions.spec.js
+++ __tests__/actions/newsActions.spec.js
@@ -1,7 +1,43 @@
-import * as actions from '../../src/js/actions/newsActions';
+// import * as actions from '../../src/js/actions/newsActions';
+// import dispatcher from '../../src/js/dispatcher';
 
-describe('Test all actions: ', () => {
-  describe('', () => {
 
-  });
-});
+
+// describe('Test all action functions: ', () => {
+//   it('should be defined', () => {
+//     expect(actions).toBeDefined();
+//   });
+//   describe('How newsStore handles dispatched actions', () => {
+//     describe('On dispatch message GET_ARTICLES', () => {
+//       const action = {
+//         type: 'GET_ARTICLES',
+//         articles: ['one', 'two', 'three'],
+//       };
+//       dispatcher.dispatch(action);
+//       it('Should set its articles to those in the dispatch', () => {
+//         expect(newsStore.fetchArticles()).toHaveLength(action.articles.length);
+//       });
+//       it('Should set its articles to those in the dispatch', () => {
+//         expect(newsStore.fetchArticles()).toEqual(action.articles);
+//       });
+//     });
+// //     describe('On dispatch message GET_SOURCES', () => {
+// //       const action = {
+// //         type: 'GET_SOURCES',
+// //         sources: ['four', 'five', 'six'],
+// //       };
+// //       dispatcher.dispatch(action);
+// //       it('Should set its sources to those in the dispatch', () => {
+// //         expect(newsStore.fetchSources()).toHaveLength(action.sources.length);
+// //       });
+// //       it('Should set its sources to those in the dispatch', () => {
+// //         expect(newsStore.fetchSources()).toEqual(action.sources);
+// //       });
+// //     });
+// //   });
+// });
+
+// // function implementActions(payload) {
+// //   return payload;
+// // }
+// // dispatcher.register(implementActions);
--- __tests__/store/newsStore.spec.js
+++ __tests__/store/newsStore.spec.js
@@ -1,61 +1,79 @@
 import dispatcher from '../../src/js/dispatcher';
 import newsStore from '../../src/js/store/newsStore';
 
+jest.mock('../../src/js/dispatcher');
+const callback = dispatcher.register.mock.calls[0][0];
+
+// beforeEach(() => {
+//   console.log(callback);
+//   callback = dispatcher.register.mock.calls[0][0];
+// });
+
 describe('Test all stores: ', () => {
-  it('should be defined', () => {
-    expect(newsStore).toBeDefined();
+  const actionGetArticles = {
+    type: 'GET_ARTICLES',
+    articles: ['one', 'two', 'three'],
+  };
+  const actionGetSources = {
+    type: 'GET_SOURCES',
+    sources: ['four', 'five', 'six'],
+  };
+  const actionAuthChanged = {
+    type: 'AUTH_CHANGED',
+    user: { name: 'Itunuloluwa' },
+  };
+  const actionInvalid = {
+    type: 'AUTHED',
+    user: { name: 'Invalid action' },
+  };
+  // describe('How newsStore handles dispatched actions', () => {
+  //   describe('Its initial state should be default', () => {
+  //     it('Should have an empty array as sources.', () => {
+  //       expect(newsStore.fetchSources()).toEqual([]);
+  //     });
+  //     it('Should have an empty array as articles.', () => {
+  //       expect(newsStore.fetchArticles()).toEqual([]);
+  //     });
+  //     it('Should have its user as null', () => {
+  //       expect(newsStore.getUser()).toBeNull();
+  //     });
+  //   });
+  // });
+  describe('On dispatch message GET_ARTICLES', () => {
+    it('Should set its articles to those in the dispatch', () => {
+      callback(actionGetArticles);
+      expect(newsStore.fetchArticles())
+        .toHaveLength(actionGetArticles.articles.length);
+    });
+    it('Should set its articles to those in the dispatch', () => {
+      expect(newsStore.fetchArticles()).toEqual(actionGetArticles.articles);
+    });
   });
-  describe('How newsStore handles dispatched actions', () => {
-    describe('On dispatch message GET_ARTICLES', () => {
-      const action = {
-        type: 'GET_ARTICLES',
-        articles: ['one', 'two', 'three'],
-      };
-      dispatcher.dispatch(action);
-      it('Should set its articles to those in the dispatch', () => {
-        expect(newsStore.fetchArticles()).toHaveLength(action.articles.length);
-      });
-      it('Should set its articles to those in the dispatch', () => {
-        expect(newsStore.fetchArticles()).toEqual(action.articles);
-      });
+  describe('On dispatch message GET_SOURCES', () => {
+    callback(actionGetSources);
+    it('Should set its sources to those in the dispatch', () => {
+      expect(newsStore.fetchSources())
+      .toHaveLength(actionGetSources.sources.length);
     });
-    describe('On dispatch message GET_SOURCES', () => {
-      const action = {
-        type: 'GET_SOURCES',
-        sources: ['four', 'five', 'six'],
-      };
-      dispatcher.dispatch(action);
-      it('Should set its sources to those in the dispatch', () => {
-        expect(newsStore.fetchSources()).toHaveLength(action.sources.length);
-      });
-      it('Should set its sources to those in the dispatch', () => {
-        expect(newsStore.fetchSources()).toEqual(action.sources);
-      });
+    it('Should set its sources to those in the dispatch', () => {
+      expect(newsStore.fetchSources()).toEqual(actionGetSources.sources);
     });
-    describe('On dispatch message AUTH_CHANGED', () => {
-      const action = {
-        type: 'AUTH_CHANGED',
-        user: { name: 'Itunuloluwa' },
-      };
-      dispatcher.dispatch(action);
-      it('Should set its user to that in the dispatch', () => {
-        expect(newsStore.getUser()).not.toBeNull();
-      });
-      it('Should set its user to that in the dispatch', () => {
-        expect(newsStore.getUser()).toBe(action.user);
-      });
+  });
+  describe('On dispatch message AUTH_CHANGED', () => {
+    callback(actionAuthChanged);
+    it('Should set its user to that in the dispatch', () => {
+      expect(newsStore.getUser()).not.toBeNull();
     });
-    describe('On invalid dispatch message e.g AUTHED', () => {
-      const action = {
-        type: 'AUTHED',
-        user: { name: 'Itunuloluwa' },
-      };
-      // Get initial state of the store
-      const initialStoreUserState = newsStore.user;
-      dispatcher.dispatch(action);
-      it('Should not tamper with the store state', () => {
-        expect(newsStore.getUser()).toEqual(initialStoreUserState);
-      });
+    it('Should set its user to that in the dispatch', () => {
+      expect(newsStore.getUser()).toBe(actionAuthChanged.user);
+    });
+  });
+  describe('On invalid dispatch message e.g AUTHED', () => {
+    // Get initial state of the store
+    const initialStoreUserState = newsStore.user;
+    callback(actionInvalid);
+    it('Should not tamper with the store state', () => {
+      expect(newsStore.getUser()).toEqual(initialStoreUserState);
     });
   });
 });
